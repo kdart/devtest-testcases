5f1e24253a864fc5eb8d2a371b89ab37dd920b9a
Keith Dart <keith@dartworks.biz>
2019-02-02 22:25:06 -0800

    Use the devtest repl in the interactive test.

--- testcases/examples/interactive.py
+++ testcases/examples/interactive.py
@@ -3,39 +3,12 @@
 """Demo module for showing some framework features.
 
 Provides a special test case that enters an interactive prompt within the test
-procedure. The user can then invoke any method and inspect to config and testbed
-interactively. Useful for demos and learning the API.
+procedure. The user can then invoke any method and inspect to config and
+testbed interactively. Useful for demos and learning the API.
 """
 
-import sys
-import os
-import readline
-import code
-
 from devtest.qa import bases
-
-
-class TestCaseConsole(code.InteractiveConsole):
-    def __init__(self, locals_):
-        super().__init__(locals=locals_, filename=__name__)
-        sys.ps1 = "Testcase> "
-        sys.ps2 = "....more> "
-        readline.set_history_length(1000)
-        if sys.platform == "darwin":
-            readline.parse_and_bind("^I rl_complete")
-        else:
-            readline.parse_and_bind("tab: complete")
-        readline.parse_and_bind("set horizontal-scroll-mode on")
-        readline.parse_and_bind("set page-completions on")
-        readline.set_completer_delims(" ")
-        self._historyfile = os.path.expandvars("$HOME/.hist_testcase")
-        try:
-            readline.read_history_file(self._historyfile)
-        except FileNotFoundError:
-            pass
-
-    def __del__(self):
-        readline.write_history_file(self._historyfile)
+from devtest.qa import repl
 
 
 class InteractiveTest(bases.TestCase):
@@ -73,8 +46,8 @@ class InteractiveTest(bases.TestCase):
     PREREQUISITES = []
 
     def procedure(self, argument=None):
-        cons = TestCaseConsole(locals())
-        cons.interact(banner="Now try out the methods and other functions.",
-                      exitmsg="")
+        cons = repl.InteractiveConsole(namespace=locals(), io=self.UI._io,
+                                       ps1="TestCase> ")
+        cons.interact(banner="Now try out the methods and other functions.")
 
 # vim:ts=4:sw=4:softtabstop=0:smarttab
